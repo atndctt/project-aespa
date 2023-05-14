ba42d18b4a459e4e943e6a64f589af5a7dc9565d
Atndc <133456171+atndctt@users.noreply.github.com>
2023-05-13 22:07:35 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoAlterarTema = document.getElementById("botao_alterar_tema")
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem_tema")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo_escuro");
+
+    body.classList.toggle("modo_escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/baguila.png")
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/baguila_branco.png")
+    }
+
+});
+
